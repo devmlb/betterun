5b8a0de09bb350654aaafcf9f1249fb7d4520f00
devmlb <97614168+devmlb@users.noreply.github.com>
2024-10-01 21:53:09 +0200

    Began to optimize popup code

--- internal/popup/popup.js
+++ internal/popup/popup.js
@@ -82,9 +82,9 @@ async function createPages(structure) {
         page.classList = 'page';
         page.innerHTML = `
         <header>
-            <mdui-button-icon class="back-btn" icon="arrow_back--rounded" style="margin-right: 16px;"></mdui-button-icon>
-            <h2 style="margin-block: 0px;">${pageInfos.name}</h2>
-            <div style="flex-grow: 1"></div>
+            <mdui-button-icon class="back-btn" icon="arrow_back--rounded"></mdui-button-icon>
+            <h2>${pageInfos.name}</h2>
+            <div class="growing-space"></div>
         </header>
         <div class="content">
         ${sections}
@@ -120,27 +120,23 @@ async function enableDisableMod(modId, state) {
 }
 
 async function createMods(modsList) {
-    const activeSwitch = document.getElementById("active-switch");
+    const activeSwitch = document.querySelector("#enable-switch mdui-switch");
     chrome.storage.sync.get(null, function (localSettings) {
         if (localSettings["gbs-enabled"]) {
             activeSwitch.setAttribute('checked', '');
         }
         activeSwitch.addEventListener('change', () => {
             let activeSwitchState = {};
-            let modsCards = document.querySelectorAll('.mods-card');
+            let modsCards = document.querySelectorAll('.mod-checkbox');
             if (activeSwitch.checked) {
                 activeSwitchState["gbs-enabled"] = true
-                modsCards.forEach((modCard) => {
-                    modCard.childNodes.forEach(mod => {
-                        mod.removeAttribute('disabled', '');
-                    });
+                modsCards.forEach((mod) => {
+                    mod.removeAttribute('disabled', '');
                 });
             } else {
                 activeSwitchState["gbs-enabled"] = false
-                modsCards.forEach((modCard) => {
-                    modCard.childNodes.forEach(mod => {
-                        mod.setAttribute('disabled', '');
-                    });
+                modsCards.forEach((mod) => {
+                    mod.setAttribute('disabled', '');
                 });
             }
             console.info("Updating setting with id 'gbs-enabled' to " + activeSwitchState['gbs-enabled'])
@@ -161,6 +157,7 @@ async function createMods(modsList) {
                     </div>
                     `
                     checkbox.setAttribute('id', 'mod-' + tempMod.id);
+                    checkbox.setAttribute('class', 'mod-checkbox');
                     if (localSettings[key].enabled) {
                         checkbox.setAttribute('checked', '');
                     }
@@ -192,97 +189,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     initUI()
 
-
+    setInterval(() => {
+        document.querySelector('body').classList.add('ready');
+    }, 160); // hiding the switch activation effect 
+    
 
     // const settingsCard = document.getElementById("settings");
-    const activeSwitch = document.getElementById("active-switch");
-    const activeSwitchHoverEffect = document.querySelector("#active-switch").shadowRoot.querySelector("label > div > div > mdui-ripple").shadowRoot.querySelector("div")
-    const divActiveSwitch = document.getElementById("div-active-switch");
-    // const checkUpdatesBtn = document.getElementById("check-updates-btn");
-    divActiveSwitch.addEventListener("click", function () {
-        activeSwitch.click()
-    });
+    const activeSwitch = document.querySelector("#enable-switch mdui-switch");
+    // const activeSwitchHoverEffect = document.querySelector("#active-switch").shadowRoot.querySelector("label > div > div > mdui-ripple").shadowRoot.querySelector("div")
+    const divActiveSwitch = document.querySelector("#enable-switch");
+    // // const checkUpdatesBtn = document.getElementById("check-updates-btn");
+    // divActiveSwitch.addEventListener("click", function () {
+    //     activeSwitch.click()
+    // });
     // checkUpdatesBtn.addEventListener("click", function() {
     //     checkUpdates();
     //     window.scrollTo({top: 0, behavior: 'smooth'});
     // });
-    divActiveSwitch.addEventListener("mouseover", function () {
-        activeSwitch.setAttribute('hover', '');
-        activeSwitchHoverEffect.classList.add("hover");
-    });
-    divActiveSwitch.addEventListener("mouseout", function () {
-        activeSwitch.removeAttribute('hover', '');
-        activeSwitchHoverEffect.classList.remove("hover");
-    });
-    // checkUpdates();
-    // fetch(chrome.runtime.getURL("/mods.json")).then(function (response) {
-    //     return response.json();
-    // }).then(function (modsList) {
-    //     chrome.storage.sync.get(null, function (localSettings) {
-    //         if (localSettings["active-extension"]) {
-    //             activeSwitch.setAttribute('checked', '');
-    //         }
-    //         activeSwitch.addEventListener('change', () => {
-    //             let activeSwitchState = {}
-    //             let checkboxes = settingsCard.childNodes
-    //             if (activeSwitch.checked) {
-    //                 activeSwitchState["active-extension"] = true
-    //                 checkboxes.forEach(function(item) {
-    //                     item.removeAttribute('disabled', '');
-    //                 });
-    //             } else {
-    //                 activeSwitchState["active-extension"] = false
-    //                 checkboxes.forEach(function(item) {
-    //                     item.setAttribute('disabled', '');
-    //                 });
-    //             }
-    //             console.info("Updating setting with id 'active-extension' to "+activeSwitchState['active-extension'])
-    //             chrome.storage.sync.set(activeSwitchState);
-    //         });
-    //         const currentVersion = chrome.runtime.getManifest().version;
-    //         document.getElementById("about").textContent = "À propos (v"+currentVersion+")"
-    //         for (const key in localSettings) {
-    //             if (key !== "active-extension" && key !== "updated") {
-    //                 let tempMod = getModById(key, modsList)
-    //                 if (!tempMod.hidden) {
-    //                     let checkbox = document.createElement("mdui-checkbox");
-    //                     let checkboxDivContent = document.createElement("div");
-    //                     let checkboxTitle = document.createElement("p");
-    //                     let checkboxDesc = document.createElement("p");
-    //                     let checkboxBadge = document.createElement("mdui-badge");
-    //                     checkboxBadge.setAttribute('variant', "large");
-    //                     checkboxBadge.textContent = tempMod.version
-    //                     checkboxDesc.setAttribute('class', "checkbox-desc");
-    //                     checkboxTitle.textContent = tempMod.title
-    //                     checkboxTitle.setAttribute('class', "checkbox-title");
-    //                     checkboxTitle.appendChild(checkboxBadge)
-    //                     checkboxDesc.textContent = tempMod.desc
-    //                     checkboxDivContent.appendChild(checkboxTitle);
-    //                     checkboxDivContent.appendChild(checkboxDesc);
-    //                     checkboxDivContent.setAttribute('class', "checkbox-div");
-    //                     checkbox.appendChild(checkboxDivContent);
-    //                     checkbox.setAttribute('id', tempMod.id);
-    //                     if (localSettings[key]) {
-    //                         checkbox.setAttribute('checked', '');
-    //                     }
-    //                     if (!activeSwitch.checked) {
-    //                         checkbox.setAttribute('disabled', '');
-    //                     }
-    //                     settingsCard.appendChild(checkbox);
-    //                     checkbox.addEventListener('change', () => {
-    //                         let checkboxId = checkbox.getAttribute("id")
-    //                         let checkboxState = {}
-    //                         if (checkbox.checked) {
-    //                             checkboxState[checkboxId] = true
-    //                         } else {
-    //                             checkboxState[checkboxId] = false
-    //                         }
-    //                         console.info("Updating setting with id '"+checkboxId+"' to "+checkboxState[checkboxId])
-    //                         chrome.storage.sync.set(checkboxState);
-    //                     });
-    //                 }
-    //             }
-    //         }
-    //     });
+    // divActiveSwitch.addEventListener("mouseover", function () {
+    //     activeSwitch.setAttribute('hover', '');
+    //     activeSwitchHoverEffect.classList.add("hover");
     // });
+    // divActiveSwitch.addEventListener("mouseout", function () {
+    //     activeSwitch.removeAttribute('hover', '');
+    //     activeSwitchHoverEffect.classList.remove("hover");
+    // });
+    divActiveSwitch.addEventListener('click', () => {
+        activeSwitch.click();
+    });
 });
