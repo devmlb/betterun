0fbdd81e62b920c6f91d303b85de087c1a79b1bb
devmlb <97614168+devmlb@users.noreply.github.com>
2024-11-02 14:42:33 +0100

    Improved mod uncloud-autologin

--- mods/uncloud-autologin/uncloud-autologin.js
+++ mods/uncloud-autologin/uncloud-autologin.js
@@ -1,7 +1,7 @@
 (function() {
-    if (window.location == 'https://uncloud.univ-nantes.fr/index.php/login') {
+    if (window.location.toString().includes('https://uncloud.univ-nantes.fr/index.php/login')) {
         window.location.replace('/index.php/apps/oidc_login/oidc');
-    } else if (window.location == 'https://uncloud.univ-nantes.fr/index.php/apps/files/files') {
+    } else {
         const logoutBtn = document.querySelector('li#logout>a');
         logoutBtn.setAttribute('href', 'https://intraetu.univ-nantes.fr/identification/logout')
     }
