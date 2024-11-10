2eb182d7f14a19025b8764d05a5825f411be50b2
devmlb <97614168+devmlb@users.noreply.github.com>
2024-11-10 09:20:14 +0100

    Improved mod refreshed-login

--- mods/refreshed-login/refreshed-login.js
+++ mods/refreshed-login/refreshed-login.js
@@ -12,3 +12,28 @@ windowHeader.innerHTML = `
 `;
 
 formWindow.prepend(windowHeader);
+
+
+document.querySelector('#usernameSection label').removeChild(document.querySelector('#username'));
+document.querySelector('#passwordSection label').removeChild(document.querySelector('#password'));
+const usernameInput = document.createElement('input');
+usernameInput.id = 'username';
+usernameInput.classList.add('un-input');
+usernameInput.setAttribute('placeholder', 'Identifiant');
+usernameInput.setAttribute('name', 'username');
+
+const passwordInput = document.createElement('input');
+passwordInput.id = 'password';
+passwordInput.classList.add('un-input');
+passwordInput.setAttribute('placeholder', 'Mot de passe');
+passwordInput.setAttribute('name', 'password');
+passwordInput.setAttribute('type', 'password');
+
+document.querySelector('#usernameSection').appendChild(usernameInput);
+document.querySelector('#passwordSection').appendChild(passwordInput);
+
+const revealPasswordButton = document.createElement('button');
+revealPasswordButton.classList = 'un-button un-button-filled';
+revealPasswordButton.innerHTML = '<i class="material-icons-round">visibility</i>';
+revealPasswordButton.setAttribute('type', 'button');
+document.querySelector('#passwordSection').appendChild(revealPasswordButton);
