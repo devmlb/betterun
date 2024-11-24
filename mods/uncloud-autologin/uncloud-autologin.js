31e522dafbee5302e2e034d3c373cad453ef7e5b
devmlb <97614168+devmlb@users.noreply.github.com>
2024-11-24 11:16:11 +0100

    Updated mod uncloud-autologin to support the latest CAS

--- mods/uncloud-autologin/uncloud-autologin.js
+++ mods/uncloud-autologin/uncloud-autologin.js
@@ -1,8 +1,14 @@
 (function() {
-    if (window.location.toString().includes('https://uncloud.univ-nantes.fr/index.php/login')) {
-        window.location.replace('/index.php/apps/oidc_login/oidc');
+    UNCLOUD_URL = 'https://uncloud.univ-nantes.fr';
+    const currentUrl = window.location.toString();
+    if (currentUrl.startsWith(UNCLOUD_URL + '/index.php/login')) {
+        window.location.replace(document.querySelector('#alternative-logins>a').href);
     } else {
-        const logoutBtn = document.querySelector('li#logout>a');
-        logoutBtn.setAttribute('href', 'https://intraetu.univ-nantes.fr/identification/logout')
+        try {
+            const logoutBtn = document.querySelector('li#logout>a');
+            logoutBtn.setAttribute('href', 'https://intraetu.univ-nantes.fr/identification/logout')
+        } catch (error) {
+            console.error('[BETTERUN][uncloud-autologin] Error while trying to replace the logout URL.\n' + error);
+        }   
     }
 })();
