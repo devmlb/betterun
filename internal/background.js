0f225fd6912f71e333769365d685d1c1c8986c44
devmlb <97614168+devmlb@users.noreply.github.com>
2025-05-31 22:10:54 +0200

    Added old files deleted by merge

--- internal/background.js
+++ internal/background.js
@@ -10,12 +10,12 @@ function checkPermissions() {
 chrome.runtime.onInstalled.addListener((details) => {
     checkPermissions();
     const currentVersion = chrome.runtime.getManifest().version;
-    if (details.reason == 'update') {
-        if (currentVersion == details.previousVersion) {
-            console.warn('The previous version of the extension (' + details.previousVersion + ') and the current version (' + currentVersion + ') are identical, despite an update. Has the version been updated in the manifest?')
+    if (details.reason == "update") {
+        if (currentVersion === details.previousVersion) {
+            console.warn(`The previous version of the extension (${details.previousVersion}) and the current version (${currentVersion}) are identical, despite an update. Has the version been updated in the manifest?`);
         } else {
-            console.log('The extension has been updated, opening the update page.');
-            chrome.tabs.create({ url: chrome.runtime.getURL('/internal/update/update.html') });
+            console.log("The extension has been updated, opening the update page.");
+            chrome.tabs.create({ url: chrome.runtime.getURL("/internal/update/update.html") });
         }
         initStorage(details.previousVersion, currentVersion);
     } else {
