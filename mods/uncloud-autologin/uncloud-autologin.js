13bcb689d7fca1a06aafd7abaf8e02f59b235185
devmlb <97614168+devmlb@users.noreply.github.com>
2025-10-11 21:09:37 +0200

    [uncloud-autologin] Fixed logout

--- mods/uncloud-autologin/uncloud-autologin.js
+++ mods/uncloud-autologin/uncloud-autologin.js
@@ -1,14 +1,14 @@
 (function() {
-    UNCLOUD_URL = 'https://uncloud.univ-nantes.fr';
+    UNCLOUD_URL = "https://uncloud.univ-nantes.fr";
     const currentUrl = window.location.toString();
-    if (currentUrl.startsWith(UNCLOUD_URL + '/index.php/login')) {
-        window.location.replace(document.querySelector('#alternative-logins>a').href);
-    } else {
-        try {
-            const logoutBtn = document.querySelector('li#logout>a');
-            logoutBtn.setAttribute('href', 'https://intraetu.univ-nantes.fr/identification/logout')
-        } catch (error) {
-            console.error('[BETTERUN][uncloud-autologin] Error while trying to replace the logout URL.\n' + error);
-        }   
+    if (currentUrl.startsWith(UNCLOUD_URL + "/index.php/login")) {
+        // Login page
+        document.body.style.display = "none";
+        window.location.replace(document.querySelector("#alternative-logins>a").href);
+    } else if (currentUrl === UNCLOUD_URL + "/index.php/login?clear=1") {
+        // Login page after logout
+        // The user is disconnected from Nextcloud but not from the CAS, 
+        // so we navigate to the CAS logout URL.
+        window.location.replace("https://intraetu.univ-nantes.fr/identification/logout");
     }
 })();
